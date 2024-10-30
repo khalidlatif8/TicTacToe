9a48311adc90de433639b141d51431dab1c5a947
Khalid Abdul Latif <khalid.alateef98@gmail.com>
2024-10-31 01:03:03 +0500

    Initial commit

---
+++ TicTacToo/app.js
@@ -0,0 +1,103 @@
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector("#reset-btn");
+const newGameBtn = document.querySelector("#new-btn");
+const msgContainer = document.querySelector(".msg-container");
+const msg = document.querySelector("#msg");
+
+let turnO = true; // playerO starts
+let count = 0; // To Track Draw
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      box.disabled = true;
+      count++;
+      let isWinner = checkWinner();
+      
+      if (!isWinner && count < 9) {
+        turnO = false; // Switch to computer's turn
+        computerMove();
+      }
+    }
+  });
+});
+
+const computerMove = () => {
+  let availableBoxes = Array.from(boxes).filter(box => box.innerText === "");
+  
+  if (availableBoxes.length > 0) {
+    const randomBox = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
+    randomBox.innerText = "X"; // Computer plays as X
+    randomBox.disabled = true;
+    count++;
+    
+    let isWinner = checkWinner();
+    if (!isWinner && count === 9) {
+      gameDraw();
+    } else {
+      turnO = true; // Switch back to player's turn
+    }
+  }
+};
+
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
